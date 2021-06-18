6a6c3257b599d32faa54205cf378e2f4ff94fbca
nthp999 <49775388+nthp999@users.noreply.github.com>
2021-06-19 00:44:28 +0700

    Update hooking App context

---
+++ app/src/main/java/com/kltn/hookdemo/hooking/ActivityContext.java
@@ -0,0 +1,40 @@
+package com.kltn.hookdemo.hooking;
+
+import android.app.Activity;
+import android.app.AndroidAppHelper;
+import android.content.Context;
+import android.util.Log;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class ActivityContext {
+    private static String TAG = "KLTN2021";
+    private static Activity mCurrentActivity;
+
+    Context context = (Context) AndroidAppHelper.currentApplication();
+
+    public void start (XC_LoadPackage.LoadPackageParam lpparam){
+        Class<?> instrumentation = XposedHelpers.findClass(
+                "android.app.Instrumentation", lpparam.classLoader);
+
+        XposedBridge.hookAllMethods(instrumentation, "newActivity", new XC_MethodHook() {
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+
+                mCurrentActivity = (Activity) param.getResult();
+                Log.e(TAG, "Current Activity : " + mCurrentActivity.getClass().getName());
+                Context ctx = (Context) mCurrentActivity;
+            }
+        });
+    }
+
+    public static Activity getCurrentActivity() {
+        Log.e(TAG, "Current Activity1 : " + mCurrentActivity.getClass().getName());
+        return mCurrentActivity;
+    }
+
+}
