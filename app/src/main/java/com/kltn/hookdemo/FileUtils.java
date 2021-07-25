8d956df5801672c65cd6d26bae12f35abe3d35e2
nthp999 <49775388+nthp999@users.noreply.github.com>
2021-07-25 22:38:42 +0700

    Update Readfile

--- app/src/main/java/com/kltn/hookdemo/FileUtils.java
+++ app/src/main/java/com/kltn/hookdemo/FileUtils.java
@@ -13,6 +13,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import de.robv.android.xposed.SELinuxHelper;
@@ -23,28 +25,16 @@ public class FileUtils {
     private static final String DATA_DIRECTORY = "/data/data";
     private static final String SCAN_FLAG_FILENAME = "files";
     private static final String RESULTS_FILENAME = "test.txt";
+    private static boolean CHECK_FLAG = false;
 
-    /**
-     *
-     * Checks if the flag file exists in the private application directory
-     * indicating that the application should be scanned for URLs used
-     *
-     * @param packageName: package name of the current application
-     * @return Returns true if the application should be scanned for URLs used
-     */
     public static boolean shouldScan(String packageName) {
         BaseService baseService = SELinuxHelper.getAppDataFileService();
         return baseService.checkFileExists(DATA_DIRECTORY + "/" + packageName + "/" + SCAN_FLAG_FILENAME);
     }
 
-    /**
-     * Appends a string to results file if the string is not contained in the file yet
-     *
-     * @param packageName:   package name of the hooking application
+    public static boolean saveStringToResultsFile(String packageName, String inputURL) {
 
-     */
-    public static String [] saveStringToResultsFile(String packageName) {
-        String[] blacklist = new String[100];
+
         try {
             //Context mContext = ActivityContext.getCurrentActivity().getApplicationContext();
             File resultsFile = new File(DATA_DIRECTORY + "/" + packageName + "/" + SCAN_FLAG_FILENAME + "/" + RESULTS_FILENAME);
@@ -52,10 +42,11 @@ public class FileUtils {
 
             //Create results file if not present
             if (!resultsFile.exists()) {
+                Log.e("KLTN2021: ", "Access Failed");
                 boolean created = resultsFile.createNewFile();
                 if (!created) {
                     XposedBridge.log("Failed to create results file. Path: " + resultsFile.getAbsolutePath());
-                    return null;
+                    //return false;
                 }
             }
 
@@ -63,38 +54,32 @@ public class FileUtils {
             FileReader fr = new FileReader(resultsFile);
             BufferedReader br = new BufferedReader(fr);
 
-            //StringBuilder fileContentBuilder = new StringBuilder();
+            StringBuilder fileContentBuilder = new StringBuilder();
             String line;
-            int i = 0;
             while ((line = br.readLine()) != null) {
-                //fileContentBuilder.append(line);
-                //blacklist.add(line);
-                blacklist[i] = line;
-                i++;
+                fileContentBuilder.append(line);
                 Log.e("KLTN2021", "line: " + line);
             }
 
             fr.close();
             br.close();
 
-            Log.e("KLTN2021", "Get String array in FuleUtils: " + blacklist.length);
-
-            //String fileContent = fileContentBuilder.toString();
+            String fileContent = fileContentBuilder.toString();
             //Log.e("KLTN2021: ", fileContent);
             //Skip if the file already contains the string
-           /* if (fileContent.contains(inputURL)) {
-                Log.e("KLTN2021", "Doc va phat hien duoc du lieu nhay cam");
-
+            if (fileContent.contains(inputURL)) {
+               Log.e("KLTN2021", "Doc va phat hien duoc du lieu nhay cam");
+               CHECK_FLAG = true;
                 //Append url to the file
                 //FileWriter fw = new FileWriter(resultsFile, true);
                 //BufferedWriter bw = new BufferedWriter(fw);
                 //bw.write(stringToWrite + "\n");
                 //bw.close();
-            }*/
+            }
 
         } catch (IOException e) {
             XposedBridge.log(e.getMessage());
         }
-        return blacklist;
+        return CHECK_FLAG;
     }
 }
